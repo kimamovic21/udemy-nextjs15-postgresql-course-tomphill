4a13ad99e0c1c7355e290e4ac0ae0ac148e4bda8
Kerim Imamović <imamovic.kerim@gmail.com>
2025-05-06 16:16:40 +0200

    Finish rendering the remaining form fields

--- components/transactions/transaction-form.tsx
+++ components/transactions/transaction-form.tsx
@@ -3,6 +3,9 @@
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { format } from 'date-fns';
+import { Calendar as CalendarIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
 import { transactionFormSchema } from '@/lib/validators';
 import { defaultValues } from '@/lib/constants';
 import {
@@ -10,7 +13,8 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel
+  FormLabel,
+  FormMessage,
 } from '../ui/form';
 import {
   Select,
@@ -19,6 +23,14 @@ import {
   SelectTrigger,
   SelectValue
 } from '../ui/select';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Input } from '../ui/input';
 
 const TransactionForm = () => {
   const form = useForm<z.infer<typeof transactionFormSchema>>({
@@ -29,13 +41,13 @@ const TransactionForm = () => {
   const handleSubmit = async (
     data: z.infer<typeof transactionFormSchema>
   ) => {
-
+    console.log(data);
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <fieldset className='grid grid-cols-2 gap-y-5 gap-x-2'>
+        <fieldset className='grid grid-cols-2 gap-y-5 gap-x-2 items-start'>
           <FormField
             control={form.control}
             name='transactionType'
@@ -63,11 +75,12 @@ const TransactionForm = () => {
                       </SelectContent>
                     </Select>
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )
             }}
           />
-          
+
           <FormField
             control={form.control}
             name='categoryId'
@@ -86,13 +99,108 @@ const TransactionForm = () => {
                         <SelectValue />
                       </SelectTrigger>
                       <SelectContent>
+                        <SelectItem value='1'>
+                          Salary
+                        </SelectItem>
+                        <SelectItem value='2'>
+                          Investments
+                        </SelectItem>
                       </SelectContent>
                     </Select>
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )
             }}
           />
+
+          <FormField
+            control={form.control}
+            name='transactionDate'
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Transaction Date
+                  </FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'justify-start text-left font-normal',
+                            !field.value && 'text-muted-foreground'
+                          )}
+                        >
+                          <CalendarIcon className='mr-2 h-4 w-4' />
+                          {field.value ? (
+                            format(field.value, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className='w-auto p-0'>
+                        <Calendar
+                          mode='single'
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                          disabled={{
+                            after: new Date(),
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <FormField
+            control={form.control}
+            name='amount'
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Amount
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} type='number' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+        </fieldset>
+
+        <fieldset className='mt-5 flex flex-col gap-5'>
+          <FormField
+            control={form.control}
+            name='description'
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>
+                    Description
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
+
+          <Button type='submit'>
+            Submit
+          </Button>
         </fieldset>
       </form>
     </Form>
